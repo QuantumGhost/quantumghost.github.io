f2488d0719563f965cc63ddc92a529fbba814277
QuantumGhost <obelisk.reg+git@gmail.com>
2025-02-22 00:12:10 +0800

    fix: 修复测试环境下跳转路径的 base url 不对的问题

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,10 @@ set -o pipefail
 SCRIPT_DIR="$(realpath $(dirname $0))"
 REPO_ROOT="$(dirname "${SCRIPT_DIR}")"
 
-hugo build --gc
+BASE_URL='https://blog.quantumghost.dev/'
+if [ -n "${VERCEL_URL}" ]; then
+    BASE_URL="https://${VERCEL_URL}"
+fi
+
+hugo build --gc --baseURL "${BASE_URL}"
 cp "${REPO_ROOT}/public/index.xml" "${REPO_ROOT}/public/all.atom.xml"
